a8da8124b825f6777a302ef8c865ddff370f3320
yahiro <yahiro07@users.noreply.github.com>
2026-05-24 09:35:34 +0900

    fetch units

--- proto0/vite.config.ts
+++ proto0/vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { unitLoaderPlugin } from "wus-vite-unit-loader-plugin";
+import { unitSourceUrls } from "./src/unit-source-urls";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    unitLoaderPlugin({ unitSourceUrls, cacheFolderPath: "./.wus-cache" }),
+  ],
   appType: "mpa",
   resolve: { tsconfigPaths: true },
   server: { port: 3004 },
