aa99bfeb64514f01709a4135dc474180d33a5e39
yahiro <yahiro07@users.noreply.github.com>
2026-05-28 15:30:37 +0900

    show thumbnails

--- proto0/src/index.tsx
+++ proto0/src/index.tsx
@@ -2,4 +2,5 @@ import "./styles";
 //
 // import "./x/dev0-unit-check";
 // import "./x/dev1-slots-layout";
-import "./x/dev2-capture-thumbnail";
+// import "./x/dev2-capture-thumbnail";
+import "./x/dev3-show-thumbnails";
