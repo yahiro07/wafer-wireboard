473890fe4f2615023e0d06916ecb497ed53b24c4
yahiro <yahiro07@users.noreply.github.com>
2026-06-01 09:05:43 +0900

    add labels

--- wireboard/src/features/system-port/system-control-ui.tsx
+++ wireboard/src/features/system-port/system-control-ui.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/button";
 import { Icons } from "@/components/icons";
 import { Knob } from "@/components/knob";
 import { NumberSliderBox } from "@/components/number-slider-box";
+import { UpperLabel } from "@/components/upper-label";
 import { actions } from "@/store/actions";
 import { store } from "@/store/store";
 
@@ -10,18 +11,20 @@ export const SystemControlUiA = () => {
   const { playing, bpm } = store.useSnapshot();
   return (
     <ScalerBoxAutoSized>
-      <div className="flex-ha gap-4 p-4">
+      <div className="flex-ha gap-4 pt-4 pb-1.5 px-4">
         <Button active={playing} onClick={actions.togglePlayState}>
           <Icons.Play size={20} />
         </Button>
-        <NumberSliderBox
-          value={bpm}
-          min={60}
-          max={180}
-          step={1}
-          onChange={actions.setBpm}
-          fracDigits={0}
-        />
+        <UpperLabel label="BPM">
+          <NumberSliderBox
+            value={bpm}
+            min={60}
+            max={180}
+            step={1}
+            onChange={actions.setBpm}
+            fracDigits={0}
+          />
+        </UpperLabel>
       </div>
     </ScalerBoxAutoSized>
   );
@@ -31,8 +34,10 @@ export const SystemControlUiB = () => {
   const { masterVolume } = store.useSnapshot();
   return (
     <ScalerBoxAutoSized>
-      <div className="flex-ha gap-2 p-3">
-        <Knob value={masterVolume} onChange={actions.setMasterVolume} />
+      <div className="flex-ha gap-2 pt-5.5 pb-2.5">
+        <UpperLabel label="MASTER" yOffset={-1}>
+          <Knob value={masterVolume} onChange={actions.setMasterVolume} />
+        </UpperLabel>
       </div>
     </ScalerBoxAutoSized>
   );
