2963c8ba22ec4d30aeb9cae46413a9a9791fe503
yahiro <yahiro07@users.noreply.github.com>
2026-06-02 21:57:08 +0900

    place units in absolute coord

--- wireboard_proto2/src/host-app/app.tsx
+++ wireboard_proto2/src/host-app/app.tsx
@@ -1,88 +1,123 @@
 import { mountAppRoot } from "beams/ax-react/mount-app-root";
+import { Point } from "beams/ax-ui/common-types";
+import { ReactUnitTemplateFn } from "@/framework/unit-frame/react-unit-interface";
 import { UnitBox } from "@/host-app/organisms/unit-box";
 import { reactUnitFactories } from "@/units/react";
 
-const App = () => {
-  const uf = reactUnitFactories;
-  return (
-    <div className="flex-vc gap-4">
-      <div className="flex-h gap-4">
-        <UnitBox unitId="mixer1" unitTemplateFn={uf.mixer} destSpec="$output" />
-      </div>
-      <div className="flex-h gap-4">
-        <UnitBox
-          unitId="osc1"
-          pageUrl="/units/osc/index.html"
-          destSpec="mixer1.port0"
-        />
-        <UnitBox
-          unitId="osc2"
-          unitTemplateFn={uf.osc}
-          destSpec="mixer1.port1"
-        />
-        <UnitBox
-          unitId="cvGateOsc1"
-          unitTemplateFn={uf.cvGateOsc}
-          destSpec="mixer1.port2"
-        />
-      </div>
-      <div className="flex-h gap-4">
-        <UnitBox
-          unitId="keyboard1"
-          unitTemplateFn={uf.keyboard}
-          destSpec="osc1"
-        />
-        <UnitBox
-          unitId="keyboard2"
-          unitTemplateFn={uf.keyboard}
-          destSpec="osc2"
-        />
-        <UnitBox
-          unitId="keyboard3"
-          unitTemplateFn={uf.keyboard}
-          destSpec="cvGateOsc1"
-        />
-        <UnitBox
-          unitId="stateSwitcher1"
-          unitTemplateFn={uf.stateSwitcher}
-          destSpec="osc2"
-        />
-      </div>
-      <div className="flex-ha gap-4">
-        <UnitBox
-          unitId="twoPortsKeyboard1"
-          unitTemplateFn={uf.twoPortsKeyboard}
-          destSpec={["osc1", "osc2"]}
-        />
-        <UnitBox
-          unitId="paramController1"
-          unitTemplateFn={uf.parametersController}
-          destSpec="osc2"
-        />
-        <UnitBox
-          unitId="cvGateStepSequencer1"
-          unitTemplateFn={uf.cvGateStepSequencer}
-          destSpec="cvGateOsc1"
-        />
-      </div>
+type UnitItem = {
+  unitId: string;
+  pageUrl?: string;
+  unitTemplateFn?: ReactUnitTemplateFn;
+  destSpec?: string | string[];
+  position: Point;
+};
 
-      <div className="flex-ha gap-4">
-        <UnitBox
-          unitId="masterClock1"
-          unitTemplateFn={uf.masterClock}
-          destSpec="clockDivider1"
-        />
-        <UnitBox
-          unitId="keyboard4"
-          unitTemplateFn={uf.keyboard}
-          destSpec={["osc1", "osc2"]}
-        />
-        <UnitBox
-          unitId="clockDivider1"
-          unitTemplateFn={uf.clockDivider}
-          destSpec="cvGateStepSequencer1"
-        />
-      </div>
+const uf = reactUnitFactories;
+
+const unitItems: UnitItem[] = [
+  {
+    unitId: "mixer1",
+    unitTemplateFn: uf.mixer,
+    destSpec: "$output",
+    position: { x: 400, y: 0 },
+  },
+  {
+    unitId: "osc1",
+    pageUrl: "/units/osc/index.html",
+    destSpec: "mixer1.port0",
+    position: { x: 100, y: 150 },
+  },
+  {
+    unitId: "osc2",
+    unitTemplateFn: uf.osc,
+    destSpec: "mixer1.port1",
+    position: { x: 400, y: 180 },
+  },
+  {
+    unitId: "cvGateOsc1",
+    unitTemplateFn: uf.cvGateOsc,
+    destSpec: "mixer1.port2",
+    position: { x: 700, y: 150 },
+  },
+  {
+    unitId: "keyboard1",
+    unitTemplateFn: uf.keyboard,
+    destSpec: "osc1",
+    position: { x: 100, y: 400 },
+  },
+  {
+    unitId: "keyboard2",
+    unitTemplateFn: uf.keyboard,
+    destSpec: "osc2",
+    position: { x: 400, y: 400 },
+  },
+  {
+    unitId: "keyboard3",
+    unitTemplateFn: uf.keyboard,
+    destSpec: "cvGateOsc1",
+    position: { x: 700, y: 400 },
+  },
+  {
+    unitId: "stateSwitcher1",
+    unitTemplateFn: uf.stateSwitcher,
+    destSpec: "osc2",
+    position: { x: 1000, y: 400 },
+  },
+  {
+    unitId: "twoPortsKeyboard1",
+    unitTemplateFn: uf.twoPortsKeyboard,
+    destSpec: ["osc1", "osc2"],
+    position: { x: 100, y: 600 },
+  },
+  {
+    unitId: "paramController1",
+    unitTemplateFn: uf.parametersController,
+    destSpec: "osc2",
+    position: { x: 400, y: 600 },
+  },
+  {
+    unitId: "cvGateStepSequencer1",
+    unitTemplateFn: uf.cvGateStepSequencer,
+    destSpec: "cvGateOsc1",
+    position: { x: 700, y: 600 },
+  },
+  {
+    unitId: "masterClock1",
+    unitTemplateFn: uf.masterClock,
+    destSpec: "clockDivider1",
+    position: { x: 100, y: 800 },
+  },
+  {
+    unitId: "keyboard4",
+    unitTemplateFn: uf.keyboard,
+    destSpec: ["osc1", "osc2"],
+    position: { x: 400, y: 800 },
+  },
+  {
+    unitId: "clockDivider1",
+    unitTemplateFn: uf.clockDivider,
+    destSpec: "cvGateStepSequencer1",
+    position: { x: 700, y: 800 },
+  },
+];
+
+const App = () => {
+  return (
+    <div className="flex-h gap-4 relative">
+      {unitItems.map((item) => (
+        <div
+          key={item.unitId}
+          className="absolute"
+          style={{ left: item.position.x, top: item.position.y }}
+        >
+          <UnitBox
+            unitId={item.unitId}
+            unitTemplateFn={item.unitTemplateFn}
+            pageUrl={item.pageUrl}
+            destSpec={item.destSpec}
+          />
+        </div>
+      ))}
     </div>
   );
 };
