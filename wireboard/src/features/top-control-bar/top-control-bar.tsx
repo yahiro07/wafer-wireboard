427d44b38e09d917355db80b1a9588de155803a2
yahiro <yahiro07@users.noreply.github.com>
2026-06-07 11:02:35 +0900

    fix pointer area

--- wireboard/src/features/top-control-bar/top-control-bar.tsx
+++ wireboard/src/features/top-control-bar/top-control-bar.tsx
@@ -11,8 +11,8 @@ import { store } from "@/store/store";
 export const TopControlBar = () => {
   const { playing, bpm, masterVolume } = store.useSnapshot();
   return (
-    <div className="absolute top-0 left-0 w-full p-1 flex-c">
-      <div className="bg-gray-500 flex-c px-4 gap-4 pt-3.5 pb-1.5">
+    <div className="absolute top-0 left-0 w-full p-1 flex-c pointer-events-none">
+      <div className="bg-gray-500 flex-c px-4 gap-4 pt-3.5 pb-1.5 pointer-events-auto">
         <div className="flex-ha gap-4">
           <Button active={playing} onClick={actions.togglePlayState}>
             <Icons.Play size={20} />
