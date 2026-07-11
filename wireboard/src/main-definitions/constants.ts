6e9d9500da63b723af96daea670b9f5d165bd1cd
yahiro <yahiro07@users.noreply.github.com>
2026-07-11 23:47:19 +0900

    adjust wire color

--- wireboard/src/main-definitions/constants.ts
+++ wireboard/src/main-definitions/constants.ts
@@ -3,8 +3,8 @@ export const boardSize = { width: 9000, height: 6000 };
 export const domEditAreaId = "domEditMainLayer";
 
 export const signalColors = {
-  audio: "#e48",
-  note: "#4ae",
+  audio: "#fc0",
+  note: "#0be",
   clock: "#ec4",
-  automation: "#95d",
+  automation: "#c4f",
 };
