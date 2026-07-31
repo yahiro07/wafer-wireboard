34833f9f5233e42b4bc13e42dcfe7d3f271a09c0
yahiro <yahiro07@users.noreply.github.com>
2026-07-31 09:52:19 +0900

    affect api change

--- wireboard/src/periphery/song-key-support.ts
+++ wireboard/src/periphery/song-key-support.ts
@@ -1,47 +1,27 @@
 import { useEffect } from "react";
+import { SongKeySpec } from "wafer-host/unit-types";
 import { hostSystem } from "@/model/host-system-instance";
 import { store } from "@/model/store";
 
-const keyTransposeMap = {
+const rootMap = {
   G: -5,
   "G#": -4,
-  Ab: -4,
   A: -3,
   "A#": -2,
-  Bb: -2,
   B: -1,
   C: 0,
   "C#": 1,
-  Db: 1,
   D: 2,
   "D#": 3,
-  Eb: 3,
   E: 4,
   F: 5,
   "F#": 6,
-  Gb: 6,
-  //
-  Em: -5,
-  Fm: -4,
-  "F#m": -3,
-  Gbm: -3,
-  Gm: -2,
-  "G#m": -1,
-  Abm: -1,
-  Am: 0,
-  "A#m": 1,
-  Bbm: 1,
-  Bm: 2,
-  Cm: 3,
-  "C#m": 4,
-  Dbm: 4,
-  Dm: 5,
-  "D#m": 6,
-  Ebm: 6,
 };
 
-function getKeyTranspose(songKey: string) {
-  return keyTransposeMap[songKey as keyof typeof keyTransposeMap] ?? 0;
+function getSongKeySpec(songKey: string): SongKeySpec {
+  const mode = songKey.includes("m") ? "minor" : "major";
+  const root = rootMap[songKey.replace("m", "") as keyof typeof rootMap] ?? 0;
+  return { root, mode };
 }
 
 export function useSetupSongKeySupport() {
@@ -50,9 +30,8 @@ export function useSetupSongKeySupport() {
       if (ev.type === "unitAdded") {
         const { songKey, bpm } = store.state;
         const unit = ev.unitInstance;
-        const keyTranspose = getKeyTranspose(songKey);
-        unit.hostCallbacks?.setKey?.(songKey);
-        unit.hostCallbacks?.setKeyTranspose?.(keyTranspose);
+        const songKeySpec = getSongKeySpec(songKey);
+        unit.hostCallbacks?.setKey?.(songKeySpec);
         unit.hostCallbacks?.setBpm?.(bpm);
       }
     });
@@ -60,10 +39,9 @@ export function useSetupSongKeySupport() {
 
   const { songKey } = store.useSnapshot();
   useEffect(() => {
+    const songKeySpec = getSongKeySpec(songKey);
     hostSystem.getAllUnits().forEach((unit) => {
-      const keyTranspose = getKeyTranspose(songKey);
-      unit.hostCallbacks?.setKey?.(songKey);
-      unit.hostCallbacks?.setKeyTranspose?.(keyTranspose);
+      unit.hostCallbacks?.setKey?.(songKeySpec);
     });
   }, [songKey]);
 }
