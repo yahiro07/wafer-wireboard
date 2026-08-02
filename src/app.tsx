ea4cc6f2f037833d78e58dfcd06fe34b99835bff
yahiro <yahiro07@users.noreply.github.com>
2026-08-03 02:36:43 +0900

    enabled dynamic clocking

--- src/app.tsx
+++ src/app.tsx
@@ -41,7 +41,7 @@ const App = () => {
   useEffect(projectsModel.setupLifecycle, []);
   useSequencerTickDriverRunner({ sequencerTickDriver, playing, bpm });
   useEffect(() => {
-    if (0) {
+    if (1) {
       return setupDynamicClockingSupport(); //unstable so far
     }
   }, []);
