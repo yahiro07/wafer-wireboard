c2ba07a9c3e0196c1846784f8c0ab48813f5e0d9
yahiro <yahiro07@users.noreply.github.com>
2026-08-15 20:48:09 +0900

    add extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { unitLoaderPlugin } from "wafer-host/vite-plugin";
-import { unitSourceUrls } from "./src/unit-source-urls";
+import { unitSourceUrls } from "./src/unit-source-urls.ts";
 
 export default defineConfig({
   appType: "mpa",
