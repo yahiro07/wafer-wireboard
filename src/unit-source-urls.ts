6dd8beab8db346a28969843d6e7204c6d8e9462c
yahiro <yahiro07@users.noreply.github.com>
2026-08-17 02:51:27 +0900

    refer online units

--- src/unit-source-urls.ts
+++ src/unit-source-urls.ts
@@ -3,9 +3,9 @@ function toAbsolutePath(path: string): string {
 }
 
 function getUnitSourceUrls() {
-  if (0) {
+  if (1) {
     //release
-    const waferUnitsBase = `https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r19`;
+    const waferUnitsBase = `https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r20`;
     const waferCustomUnitsBase = `https://cdn.jsdelivr.net/gh/yahiro07/wafer-custom-units@r19`;
 
     return [
