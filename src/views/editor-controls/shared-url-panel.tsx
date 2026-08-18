06d1ea350b0912fa7890b394c23617d90bd283cf
yahiro <yahiro07@users.noreply.github.com>
2026-08-19 06:16:16 +0900

    add open button

--- src/views/editor-controls/shared-url-panel.tsx
+++ src/views/editor-controls/shared-url-panel.tsx
@@ -6,16 +6,23 @@ import { projectsModel } from "@/project/projects-model";
 export const SharedUrlPanel = () => {
   const url = useMemo(projectsModel.emitSharedUrl, []);
 
-  const closePane = () => actions.hideModalPanel();
+  const closePanel = () => actions.hideModalPanel();
+
+  const handleOpen = () => {
+    window.open(url, "_blank");
+    closePanel();
+  };
   const handleCopy = async () => {
     await navigator.clipboard.writeText(url);
     window.alert("Copied to clipboard");
+    closePanel();
   };
+
   // const sizeText = `${(url.length / 1024).toFixed(1)} KB`;
   const sizeText = `${url.length} bytes`;
 
   return (
-    <div className="absolute-full z-10 flex-v items-end" onClick={closePane}>
+    <div className="absolute-full z-10 flex-v items-end" onClick={closePanel}>
       <div
         className="w-[600px] bg-gray-100 m-4 p-4 py-6"
         onClick={(e) => e.stopPropagation()}
@@ -24,7 +31,10 @@ export const SharedUrlPanel = () => {
         <div className="flex-v gap-2">
           <div className="flex-ha justify-between">
             <h3>Shared URL</h3>
-            <Button onClick={handleCopy}>Copy</Button>
+            <div className="flex-ha gap-2">
+              <Button onClick={handleCopy}>Copy</Button>
+              <Button onClick={handleOpen}>Open</Button>
+            </div>
           </div>
           <textarea
             className="w-full border border-gray-300 p-2 break-all bg-white"
