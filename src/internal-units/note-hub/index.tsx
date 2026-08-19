dcce7ab03075fe0868409a8a012f1ebf3d064489
yahiro <yahiro07@users.noreply.github.com>
2026-08-20 06:23:55 +0900

    show note number

--- src/internal-units/note-hub/index.tsx
+++ src/internal-units/note-hub/index.tsx
@@ -5,18 +5,31 @@ import { hmrActions } from "@/periphery/hmr-handler";
 
 export const createNoteHubUnit: ReactUnitTemplateFn = (unitInterface) => {
   const noteOutputPort = unitInterface.createNoteOutputPort();
+  const ac = unitInterface.audioContext;
 
   const store = createStore<{ notes: number[] }>({
     notes: [],
   });
+  function scheduleAction(time: number, fn: () => void) {
+    const waitTime = Math.max(time ?? 0, ac.currentTime) - ac.currentTime;
+    if (waitTime <= 0) {
+      fn();
+    } else {
+      setTimeout(fn, waitTime * 1000);
+    }
+  }
   const actions = {
     noteOn(note: number, time: number, vel: number) {
-      store.setNotes((prev) => [...prev, note]);
       noteOutputPort.noteOn(note, time, vel);
+      scheduleAction(time, () => {
+        store.setNotes((prev) => [...prev, note]);
+      });
     },
     noteOff(note: number, time: number) {
-      store.setNotes((prev) => prev.filter((n) => n !== note));
       noteOutputPort.noteOff(note, time);
+      scheduleAction(time, () => {
+        store.setNotes((prev) => prev.filter((n) => n !== note));
+      });
     },
   };
   unitInterface.completeSetup({
@@ -30,11 +43,17 @@ export const createNoteHubUnit: ReactUnitTemplateFn = (unitInterface) => {
   return {
     RenderUi() {
       const { notes } = store.useSnapshot();
+      const latestNote = notes[notes.length - 1];
       return (
         <div className="w-full h-full bg-white flex-c text-[#555]">
           <div className="w-[64px] h-[64px] flex-vc gap-1">
-            <div className="text-sm">note hub</div>
-            <NotesPatternCell notes={notes} />
+            <div className="text-xs">note hub</div>
+            <div className="flex-h gap-1">
+              <NotesPatternCell notes={notes} />
+              <div className="w-[20px] text-xs bd-gray-400 flex-c">
+                {latestNote}
+              </div>
+            </div>
           </div>
         </div>
       );
